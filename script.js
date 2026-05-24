d51d9f5e2180e78b1a8a6b00140d740d1b6da1b9
simochannel3-coder <simochannel3@gmail.com>
2026-05-24 01:09:54 +0100

    Refactor messages array and clean up code

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const messages = [
 
 "💌 Habibati wahoub hayaati Had nhaar mchi bhal ay nhar hit had nhar tzad 9amar dylii  ❤️",
 
-"💖 Nti hya hyati w3mri lah ykhliiiiik liyaa ❤️🥺✨",
+"💖 Nti hya hyati w3mri lah ykhliiiiik liyaa ❤️",
 
-"🌹 Mkntkhylch hyati bla bik ahbiba dyli kola 3am wnti b alf khiiir  ❤️ 💖",
+"🌹 Mkntkhylch hyati bla bik ahbiba dyli kola 3am wnti b alf khiiir  ❤️",
 
-"💕 Kanmoooot 3lik kanmoot 3la ri7aa dylk dhkt,3winaaatk w ydidatk w lips dylk Kanmot 3lik kamlaa bkol haja fik  ❤️ 🌹",
+"💕 Kanmoooot 3lik kanmoot 3la ri7aa dylk dhkt,3winaaatk w ydidatk w lips dylk Kanmot 3lik kamlaa bkol haja fik  ❤️",
 
-"✨ Nti hyaa limzynaa liya hayati wmnwrahaa bla bik had hayaat mtswa wlo  ❤️ ❤️",
+"✨ Nti hyaa limzynaa liya hayati wmnwrahaa bla bik had hayaat mtswa wlo  ❤️",
 
-"❤️ Fach knchofk frhana ktrd fiya rou7 wkaywlii nhari zwiin ajml haja w93aat fhyati hya awl nhar fkrt nhdr m3aak kanhmd lah hit hdrt m3aak hit ana db aktaar wahd fhad dnyaa frhaan hit 3ndo had jamaal whad ghzalaa fhayaatoo ❤️🥰",
+"❤️ Fach knchofk frhana ktrd fiya rou7 wkaywlii nhari zwiin ajml haja w93aat fhyati hya awl nhar fkrt nhdr m3aak kanhmd lah hit hdrt m3aak hit ana db aktaar wahd fhad dnyaa frhaan hit 3ndo had jamaal whad ghzalaa fhayaatoo ❤️",
 
-"💞 Mkrhtch fach nkon m3ak yhbs w9t hit m3aaak kanl9aa rahti wkn3iich m3aak f3alamk li mkhlinii aktr waaaahd mrtaa7 fhad dnyaa  ❤️💕",
+"💞 Mkrhtch fach nkon m3ak yhbs w9t hit m3aaak kanl9aa rahti wkn3iich m3aak f3alamk li mkhlinii aktr waaaahd mrtaa7 fhad dnyaa  ❤️",
 
-"Happy Birthday ya ajmaal whda fhad 3alaam 🥰 KANBGHIIIIIIIIIIIK WKANMOOOOOOOOOOOOOOOT 3LIIIIIIK WYKHLIIIIIIIK LIIIIIIIIYA❤️❤️"
+"🥰 KANBGHIIIIIIIIIIIK WKANMOOOOOOOOOOOOOOOT 3LIIIIIIK WYKHLIIIIIIIK LIIIIIIIIYA❤️"
 
 ];
 
@@ -49,6 +49,9 @@ if(opened >= 8){
 document.getElementById("continueBtn").classList.remove("hidden");
 
 }
+
+}
+
 function goHome(){
 
 document.getElementById("way1").classList.add("hidden");
@@ -60,4 +63,3 @@ document.getElementById("intro").classList.remove("hidden");
 window.scrollTo(0,0);
 
 }
-
